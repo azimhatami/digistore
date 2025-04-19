0742bbf436ef252dc0e1aa9dd20cdf35b257805f
Azim Hatami <azimhatami.dev@gmail.com>
2025-04-19 09:31:24 +0330

    Initial setup project

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+async function main () {
+  const app = express();
+  const port = process.env.PORT ?? 5000;
+
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+
+  // Error Handling
+  app.use((req, res, next) => {
+    return res.status(404).json({
+      message: "page not found",
+    });
+  })
+
+  app.use((error, req, res, next) => {
+    const status = error?.status ?? 500;
+    const message = error?.message ?? "Internal server error";
+    return res.status(status).json({
+      message: message,
+    });
+  })
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}: http://localhost:${port}`);
+  })
+  
+}
+
+main()
