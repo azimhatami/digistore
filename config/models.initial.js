f7be8d1989292bb38ebc62e271ae43001ed93123
Azim Hatami <azimhatami.dev@gmail.com>
2025-04-24 00:02:44 +0330

    Add basket model with relations

--- config/models.initial.js
+++ config/models.initial.js
@@ -7,6 +7,7 @@ const {
 } = require('../modules/product/product.model');
 const { User, Otp } = require('../modules/user/user.model');
 const { RefreshToken } = require('../modules/user/refreshToken.model');
+const { Basket } = require('../modules/basket/basket.model');
 
 
 async function initDatabase() {
@@ -22,6 +23,16 @@ async function initDatabase() {
   User.hasOne(Otp, {foreignKey: 'userId', as: 'otp', sourceKey: 'id'})
   Otp.hasOne(User, {foreignKey: 'otpId', as: 'user', sourceKey: 'id'})
   Otp.belongsTo(User, {foreignKey: 'userId', targetKey: 'id'})
+
+  Product.hasMany(Basket, {foreignKey: 'productId', as: 'basket', sourceKey: 'id'})
+  ProductColor.hasMany(Basket, {foreignKey: 'colorId', as: 'basket', sourceKey: 'id'})
+  ProductSize.hasMany(Basket, {foreignKey: 'sizeId', as: 'basket', sourceKey: 'id'})
+  User.hasMany(Basket, {foreignKey: 'userId', as: 'basket', sourceKey: 'id'})
+
+  Basket.belongsTo(Product, {foreignKey: 'productId', targetKey: 'id', as: 'product' })
+  Basket.belongsTo(ProductColor, {foreignKey: 'colorId', targetKey: 'id', as: 'color' })
+  Basket.belongsTo(ProductSize, {foreignKey: 'sizeId', targetKey: 'id', as: 'size' })
+  Basket.belongsTo(User, {foreignKey: 'userId', targetKey: 'id', as: 'user' })
 
   // RefreshToken.sync()
 
